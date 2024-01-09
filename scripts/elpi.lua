c4443c9fb4d226b8dba4e7ee969c537b058c9af3
Erik Nijenhuis <erik@xerdi.com>
2024-01-09 22:06:14 +0100

    Add license

--- scripts/elpi.lua
+++ scripts/elpi.lua
@@ -1,3 +1,21 @@
+-- elpi.lua
+-- Copyright 2024 E. Nijenhuis
+--
+-- This work may be distributed and/or modified under the
+-- conditions of the LaTeX Project Public License, either version 1.3c
+-- of this license or (at your option) any later version.
+-- The latest version of this license is in
+-- http://www.latex-project.org/lppl.txt
+-- and version 1.3c or later is part of all distributions of LaTeX
+-- version 2005/12/01 or later.
+--
+-- This work has the LPPL maintenance status ‘maintained’.
+--
+-- The Current Maintainer of this work is E. Nijenhuis.
+--
+-- This work consists of the files elpi.sty elpi-manual.pdf
+-- elpi.lua elpi-common.lua elpi-namespace.lua elpi-parser.lua
+-- and elpi-types.lua
 
 if not modules then
     modules = {}
@@ -5,9 +23,10 @@ end
 
 modules.elpi = {
     version = "0.0.1",
+    date = "2024/01/09",
     comment = 'Extended LaTeX Parameter Interface — for specifying and inserting document parameters',
     author = 'Erik Nijenhuis',
-    license = 'Copyright 2023 Xerdi'
+    license = 'free'
 }
 
 local api = {
